9a08068eae8341847c1bcd9473bd64561effb0e5
kushalot <38114239+kushalot@users.noreply.github.com>
2020-03-16 12:09:54 +0300

    to do app is ready толлько 1 кнопку починить

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,7 +2,8 @@ import React from "react";
 import TodoListItem from "../todo-list-item/todo-list-item";
 import './todo-list.css';
 
-const TodoList = ( {todos} ) => {
+const TodoList = ( {todos, onDeleted,
+                   onToggleImportant,onToggleDone} ) => {
 
     const elements = todos.map((item) => {
 
@@ -10,7 +11,10 @@ const TodoList = ( {todos} ) => {
 
         return (
             <li key={id} className="list-group-item">
-                <TodoListItem { ...itemProps }/>
+                <TodoListItem { ...itemProps }
+                onDeleted = { () => onDeleted(id) }
+                onToggleImportant={ () => onToggleImportant(id) }
+                onToggleDone={ () => onToggleDone(id) }/>
              </li>)
     });
 
